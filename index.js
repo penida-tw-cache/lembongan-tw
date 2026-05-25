78ca0ed1dea6a17725c22ccf37a9e22840fd1197
algosbiz <contact@algoseabiz.com>
2026-05-25 10:45:45 +0800

    update

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ class AppsScriptLogger {
       const res = await axios.post(
         APPS_SCRIPT_URL,
         { sheetName: this.sheetName, rows: this.rows },
-        { timeout: 20000, headers: { "Content-Type": "application/json" } }
+        { timeout: 60000, headers: { "Content-Type": "application/json" } }
       );
       console.log("Apps Script response:", res.status, res.data);
       if (!res.data?.ok) console.warn("Apps Script replied error:", res.data);
